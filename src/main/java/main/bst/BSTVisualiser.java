71362fdb4b3dd872fa563a33a280c498166ca690
Your Name <cooperhsmith2@gmail.com>
2025-05-26 23:15:26 +1000

    Stuck on drawnode and putting text in a circle

--- src/main/java/main/bst/BSTVisualiser.java
+++ src/main/java/main/bst/BSTVisualiser.java
@@ -10,11 +10,21 @@ import javafx.stage.Stage;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
 
+import javafx.scene.shape.Circle;
+
+
 
 
 import java.io.IOException;
 
-public class BSTVisualiser extends Application {
+public class BSTVisualiser extends Application
+{
+    double width;
+    double height;
+
+    private BST bst;
+    private Canvas canvas;
+    private GraphicsContext gc;
 
     @Override
     public void start(Stage stage) throws IOException {
@@ -35,12 +45,53 @@ public class BSTVisualiser extends Application {
 
         insert.setOnAction(e -> {
             try {
-                parse
+                int intValue = Integer.parseInt(inputfield.getText());
+                bst.insert(intValue);
+                drawtree();
 
+            } catch (NumberFormatException ex) {
+                System.out.println("invalid Number!");
             }
 
         });
+        delete.setOnAction(e -> {
+            try{
+                int intValue = Integer.parseInt(inputfield.getText());
+                bst.delete(intValue);
+                drawtree();
+            } catch (NumberFormatException ex) {
+                System.out.println("node not found!");
+            }
+        });
+
     }
+    private void drawtree() {
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        if (bst.root != null)
+        {
+            double StartX = canvas.getWidth()/2;
+            double StartY = 50;
+            double HorizontalOffset = 100;
+            double Verticalstep = 80;
+            drawNode(bst.root,StartX,StartY,HorizontalOffset, Verticalstep);
+        }
+
+    }
+
+    private void drawNode(TreeNode root, double startX, double startY, double horizontalOffset, double verticalstep) {
+        if (root == null) {
+            return;
+        }
+        else
+        {
+            Circle circle = new Circle();
+            circle.setCenterX(startX);
+            circle.setCenterY(startY);
+            
+
+        }
+    }
+
 
     public static void main(String[] args) {
         launch();
